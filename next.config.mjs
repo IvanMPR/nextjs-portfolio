562e5fa1a1a2754d4812ec2d0dc5872b2a447619
IvanMPR <ivsanmas@gmail.com>
2024-09-21 15:03:51 +0200

    Refactor GitHub Actions workflow and Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/nextjs-portfolio",
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
   images: {
     domains: ["www.codewars.com", "udemy-certificate.s3.amazonaws.com"],
     dangerouslyAllowSVG: true,
+    unoptimized: true,
   },
 };
 
