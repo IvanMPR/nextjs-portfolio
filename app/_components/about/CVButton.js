7340908a18c293371df624a053e7ded20bddbe60
IvanMPR <ivsanmas@gmail.com>
2024-09-23 17:07:41 +0200

    Refactor CVButton.js to update CV download link

--- app/_components/about/CVButton.js
+++ app/_components/about/CVButton.js
@@ -5,7 +5,7 @@ export default function CVButton() {
     <a
       // type='button'
       download='cv'
-      href='/cv.pdf'
+      href='/public/cv.pdf'
       className='block w-full rounded-full bg-secondaryColor py-[.4em]  lg:py-[.5em] mt-5 text-center dark:text-textColorDark text-textColor uppercase font-medium hover:text-textColorDark hover:dark:text-textColor transition-all'
     >
       Download CV
