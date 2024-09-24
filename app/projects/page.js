4e7fb556e30cbc5339a5506c750e896d334101ef
IvanMPR <ivsanmas@gmail.com>
2024-09-24 17:25:06 +0200

    Refactor EmblaCarousel buttons to hide on medium screens

--- app/projects/page.js
+++ app/projects/page.js
@@ -15,7 +15,9 @@ const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 export default function Projects() {
   return (
     <div className='flex flex-col items-center flex-1 w-full max-w-6xl mx-auto px-4 '>
-      <PageTitle title={"Projects"} icon={<FaUser />} />
+      <span className='hidden md:block'>
+        <PageTitle title={"Projects"} icon={<FaUser />} />
+      </span>
       <div className=' mt-16 w-full'>
         <EmblaCarousel slides={SLIDES} options={OPTIONS} />
       </div>
