0e350fca66000337dcf5c3cd4bf288fbb104f430
IvanMPR <ivsanmas@gmail.com>
2024-10-01 19:16:04 +0200

    Links corrected

--- app/_components/footer/Footer.js
+++ app/_components/footer/Footer.js
@@ -1,24 +1,29 @@
 import { FaLinkedin, FaGithub, FaLinkedinIn } from "react-icons/fa";
 import Container from "../utils/Container";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <Container>
       <footer className=' flex justify-end py-5'>
         <div className=' mr-5'>
-          <span title='Link to my portfolio page'>
-            <FaGithub
-              size={30}
-              className=' hover:cursor-pointer hover:text-primaryColor transition-all'
-            />
+          <span title='Link to my portfolio page' className='shadow-custom'>
+            <Link href='https://github.com/IvanMPR'>
+              <FaGithub
+                size={30}
+                className=' hover:cursor-pointer hover:text-primaryColor transition-all'
+              />
+            </Link>
           </span>
         </div>
         <div>
           <span title='Link to my Linkedin profile'>
-            <FaLinkedin
-              size={30}
-              className=' hover:cursor-pointer hover:text-primaryColor transition-all'
-            />
+            <Link href='https://www.linkedin.com/in/ivanmiceta'>
+              <FaLinkedin
+                size={30}
+                className=' hover:cursor-pointer hover:text-primaryColor transition-all'
+              />
+            </Link>
           </span>
         </div>
       </footer>
