b02c01abd8e1b614550cb7863a0c4498b761a6bb
IvanMPR <ivsanmas@gmail.com>
2024-10-01 19:15:29 +0200

    Link to Github and LinedIn corrected

--- app/_components/header/Header.js
+++ app/_components/header/Header.js
@@ -2,12 +2,12 @@
 
 import Logo from "./Logo";
 import ThemeSwitch from "./ThemeSwitch";
-import Navigation from "../../Navigation";
+import Navigation from "./Navigation";
 import Container from "../utils/Container";
 
 export default function Header() {
   return (
-    <header className='  shadow-custom'>
+    <header className='shadow-custom'>
       <Container>
         <div className='  flex items-center justify-between py-3 '>
           <div className=' flex items-center justify-center'>
