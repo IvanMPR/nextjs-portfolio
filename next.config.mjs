ebb4b202995eae075f1f8e746aa9bb91917097e0
IvanMPR <ivsanmas@gmail.com>
2024-10-07 19:28:00 +0200

    Refactor next.config.mjs: Remove basePath and output options

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/nextjs-portfolio",
-  output: "export", // <=== enables static exports
   reactStrictMode: true,
   images: {
     domains: ["www.codewars.com", "udemy-certificate.s3.amazonaws.com"],
