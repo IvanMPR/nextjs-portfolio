4c795b3523512ec0314e62a935d316af5d0c2c4c
IvanMPR <ivsanmas@gmail.com>
2024-10-10 17:59:21 +0200

    Path to CV file corrected

--- app/_components/about/CVButton.js
+++ app/_components/about/CVButton.js
@@ -3,9 +3,8 @@
 export default function CVButton() {
   return (
     <a
-      // type='button'
       download='cv'
-      href='/nextjs-portfolio/cv.pdf'
+      href='/cv.pdf'
       className='block w-full rounded-full bg-secondaryColor py-[.4em]  lg:py-[.5em] mt-5 text-center dark:text-textColorDark text-textColor uppercase font-medium hover:text-textColorDark hover:dark:text-textColor transition-all'
     >
       Download CV
